880948de05677613d3d7f5576ecad70fc30ce50f
Ahmad Puji Wahyono <ahmadpujiwahyono@gmail.com>
2023-11-19 18:53:46 +0700

    Update Membaca-File Text.py

--- Membaca-File.py
+++ Membaca-File.py
@@ -1,9 +1,9 @@
 users = open("users.txt", "r")
-#print(users.read())                # Untuk cetak semua isi file
-#print(users.readline())            # Untuk cetak isi file perbaris, diawali dari atas .readline
+print(users.read())                # Untuk cetak semua isi file
+print(users.readline())            # Untuk cetak isi file perbaris, diawali dari atas .readline
 array = users.readlines()           # Membuat memori array .readlines 
-#print(array)                       # Untuk cetak secara array semua isi file
-#print(array[0])                    # Untuk cetak array di baris yg di tentukan
+print(array)                       # Untuk cetak secara array semua isi file
+print(array[0])                    # Untuk cetak array di baris yg di tentukan
 
 index = 1                           # Untuk mencetak isi file sekaligus meberi index nomor                      
 for user in array:
